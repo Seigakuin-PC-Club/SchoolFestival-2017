490255057bd1a148bf97b3b97888c87ff7b934fb
Genbu Hase <hasegenbu@gmail.com>
2017-07-01 19:28:46 +0900

    うｐ。乙。

--- Core.js
+++ Core.js
@@ -2,33 +2,121 @@
  *##################################################
  *	Core.js
  *	記念祭プロジェクト in 2017の中枢コード
+ *
+ *	(前提API：DOM Extender v3.0)
  *##################################################
  *	Copyright (C) 2017 Seigakuin-PC-Club
  *##################################################
 /*/
 const SEIGPC = (function () {
 	const SEIGPC = {}; Object.defineProperties(SEIGPC, {
+		Loading: {
+			value: document.registerElement("SEIGPC-Loading", {
+				prototype: (function () {
+					document.head.appendChild(new Style({
+						"SEIGPC-Loading": {
+							"Position": "Absolute",
+							"Left": 0,
+							"Top": 0,
+							"Width": "100%",
+							"Height": "100%",
 
+							"Z-Index": -1
+						},
+						
+						"SEIGPC-Loading.is-active": {
+							"Background": "RGBA(255, 255, 255, 0.5)",
+							
+							"Z-Index": 100
+						}
+					}));
+
+
+
+					const proto = Object.create(HTMLDivElement.prototype, {
+						createdCallback: {
+							value () {
+								let loadingIcon = DOM("Div", {
+									attributes: {
+										"class": "mdl-spinner mdl-js-spinner"
+									}
+								});
+
+								this.appendChild(loadingIcon);
+							}
+						},
+
+						attachedCallback: {
+							value () {
+								
+							}
+						},
+
+						detachedCallback: {
+							value () {
+								
+							}
+						},
+
+						attributeChangedCallback: {
+							value (attr, oldValue, newValue) {
+								
+							}
+						},
+
+
+
+						start: {
+							value () {
+								this.classList.add("is-active");
+								this.querySelector("Div.mdl-spinner.mdl-js-spinner").classList.add("is-active");
+							}
+						},
+
+						stop: {
+							value () {
+								this.classList.remove("is-active");
+								this.querySelector("Div.mdl-spinner.mdl-js-spinner").classList.remove("is-active");
+							}
+						}
+					});
+
+					return proto;
+				})()
+			}),
+
+			enumerable: true
+		}
 	}); SEIGPC[Symbol.toStringTag] = "Seigakuin-PC-Club";
 
 
 
 	window.addEventListener("DOMContentLoaded", function () {
-		DOM('@A:not([Href="#"])').forEach(function (elem, index, parent) {
+		let frame = parent.document.querySelector("IFrame.mdl-layout__content");
+			frame.addEventListener("beforeunload", function () {
+				parent.document.querySelector("SEIGPC-Loading").start();
+			});
+
+			frame.addEventListener("load", function () {
+				parent.document.querySelector("SEIGPC-Loading").stop();
+			});
+
+		DOM('@A:Not([Href="#"]):Not([Disabled])').forEach(function (elem, index, p) {
 			elem.addEventListener("click", function (event) {
 				event.preventDefault();
-				DOM("$IFrame.mdl-layout__content").src = elem.href;
+
+				frame.src = elem.href;
 			});
 		});
 
-		DOM('@A[Disabled]').forEach(function (elem, index, parent) {
+		DOM('@A[Disabled]').forEach(function (elem, index, p) {
 			elem.addEventListener("click", function (event) {
 				event.preventDefault();
 			});
 		});
 	});
 
-	
+
 	
 	return SEIGPC;
 })();
