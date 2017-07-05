cc6a3c51ec2c009a5342544210b84d754cd34148
Genbu Hase <hasegenbu@gmail.com>
2017-07-05 19:57:41 +0900

    (_´Д｀)ﾉ~~ｵﾂｶﾚｰ

--- Core.js
+++ Core.js
@@ -10,6 +10,107 @@
 /*/
 const SEIGPC = (function () {
 	let baseUrl = "/SchoolFestival-2017/";
+
+	let tagTemplate = (function () {
+		let body = DOM("Body");
+			body.innerHTML = 
+			`<Template For = "SEIGPC-Loading">
+				<Style>
+					:Host {
+						Transition: Opacity 0.25s Cubic-Bezier(0.4, 0, 0.2, 1) 0s;
+
+						Position: Fixed;
+						Left: 0;
+						Top: 0;
+
+						Display: Flex;
+						Width: 100%;
+						Height: 100%;
+
+						Background: RGBA(0, 0, 0, 0.5);
+						Opacity: 0;
+						Z-Index: -1;
+						Overflow: Hidden;
+					}
+					
+					:Host(.is-active) {
+						Opacity: 1;
+						Z-Index: 1000;
+					}
+				</Style>
+
+				<Content Select = "SEIGPC-Loading-Spinner"></Content>
+			</Template>
+
+			<Template For = "SEIGPC-Loading-Spinner">
+				<Style>
+					@keyframes SEIGPC-Loading-Spinner__Animation {
+						0% {
+							Transform: Rotate(0deg);
+						}
+
+						100% {
+							Transform: Rotate(360deg);
+						}
+					}
+					
+					:Host {
+						Display: Flex;
+						Width: 100%;
+						Height: 100%;
+					}
+
+					:Host(.is-active) {
+						Animation: SEIGPC-Loading-Spinner__Animation 1s Ease-In-Out 0s Infinite Normal;
+					}
+
+					:Host::Before {
+						Content: "";
+
+						Display: Block;
+						Width: 3pc;
+						Height: 3pc;
+
+						Margin: Auto;
+
+						Background-Image: var(--Url, URL(""));
+						Background-Position: Center;
+						Background-Size: Cover;
+					}
+				</Style>
+			</Template>
+			
+			<Template For = "SEIGPC-Profile-Card">
+				<Link Rel = "StyleSheet" Href = "https://fonts.googleapis.com/icon?family=Material+Icons" />
+				<Link Rel = "StyleSheet" Href = "https://code.getmdl.io/1.3.0/material.blue_grey-pink.min.css" />
+
+				<Style>
+					:Host > Div.mdl-card__title {
+						Padding: 0;
+					}
+
+					:Host > Div.mdl-card__title ::Slotted(Img) {
+						Width: 100%;
+					}
+				</Style>
+
+				<Div Class = "mdl-card__title">
+					<Slot Name = "Picture"></Slot>
+				</Div>
+
+				<Div Class = "mdl-card__supporting-text">
+					<H2 Class = "mdl-card__title-text">
+						<Slot Name = "Name"></Slot>
+					</H2>
+				</Div>
+
+				<Div Class = "mdl-card__actions mdl-card--border">
+					<Slot Name = "Detail"></Slot>
+				</Div>
+			</Template>`;
+			
+		return body;
+	})();
 	
 	const SEIGPC = {}; Object.defineProperties(SEIGPC, {
 		Loading: {
@@ -18,20 +119,8 @@ const SEIGPC = (function () {
 					prototype: Object.create(HTMLDivElement.prototype, {
 						createdCallback: {
 							value () {
-								/*this.spinner = DOM("Div", {
-									attributes: {
-										"class": "mdl-spinner mdl-js-spinner mdl-spinner--single-color"
-									}
-								});*/
-
-								this.spinner = (function () {
-									let spinner = new SEIGPC.Loading.Spinner();
-										spinner.setImage("favicon.png");
-
-									return spinner;
-								})()
-
-								this.appendChild(this.spinner);
+								let root = this.createShadowRoot();
+									root.appendChild(document.importNode(tagTemplate.querySelector('Template[For="SEIGPC-Loading"]').content, true));
 							}
 						},
 
@@ -71,40 +160,71 @@ const SEIGPC = (function () {
 							}
 						}
 					})
-				}); document.head.appendChild(new Style({
-					"SEIGPC-Loading": {
-						"Transition": "Opacity 0.25s Cubic-Bezier(0.4, 0, 0.2, 1) 0s",
-
-						"Position": "Absolute",
-						"Left": 0,
-						"Top": 0,
-
-						"Display": "Flex",
-						"Width": "100%",
-						"Height": "100%",
-
-						"Background": "RGBA(0, 0, 0, 0.5)",
-						"Opacity": 0,
-						"Z-Index": -1,
-						"Overflow": "Hidden"
-					},
-					
-					"SEIGPC-Loading.is-active": {
-						"Opacity": 1,
-						"Z-Index": 101
-					},
-
-					"SEIGPC-Loading > Div.mdl-spinner.mdl-js-spinner": {
-						"Margin": "Auto"
-					}
-				})); Object.defineProperties(Loading, {
+				}); Object.defineProperties(Loading, {
 					Spinner: {
 						value: (function () {
 							const Spinner = document.registerElement("SEIGPC-Loading-Spinner", {
 								prototype: Object.create(HTMLDivElement.prototype, {
 									createdCallback: {
 										value () {
+											let root = this.createShadowRoot();
+												root.appendChild(document.importNode(tagTemplate.querySelector('Template[For="SEIGPC-Loading-Spinner"]').content, true));
+										}
+									},
+
+									attachedCallback: {
+										value () {
+											if (this.parentElement.nodeName == "SEIGPC-LOADING") this.parentElement.spinner = this;
+										}
+									},
+
+									detachedCallback: {
+										value () {
 											
+										}
+									},
+
+									attributeChangedCallback: {
+										value (attr, oldValue, newValue) {
+											
+										}
+									}
+								})
+							});
+
+							return Spinner;
+						})(),
+
+						enumerable: true
+					}
+				});
+
+				return Loading;
+			})(),
+
+			enumerable: true
+		},
+
+		Profile: {
+			value: (function () {
+				const Profile = Object.create(Object.prototype, {
+					Card: {
+						value: (function () {
+							const Card = document.registerElement("SEIGPC-Profile-Card", {
+								prototype: Object.create(HTMLDivElement.prototype, {
+									createdCallback: {
+										value () {
+											let root = this.attachShadow({ mode: "closed" });
+												root.appendChild(document.importNode(tagTemplate.querySelector('Template[For="SEIGPC-Profile-Card"]').content, true));
+
+											this.classList.add("mdl-card");
+											this.classList.add("mdl-shadow--4dp");
+
+											if (this.querySelector("A[Slot='Detail']")) {
+												"mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--colored".split(" ").forEach((function (elem, index, parent) {
+													this.querySelector("A[Slot='Detail']").classList.add(elem);
+												}).bind(this));
+											}
 										}
 									},
 
@@ -122,59 +242,20 @@ const SEIGPC = (function () {
 
 									attributeChangedCallback: {
 										value (attr, oldValue, newValue) {
-											switch (attr) {
-												case "src":
-													this.style.backgroundImage = "URL(" + newValue + ")";
-													break;
-											}
+											
 										}
 									},
-
-
-
-									setImage: {
-										value (src) {
-											this.src = src || "";
-											this.setAttribute("Src", src || "");
-										}
-									}
 								})
-							}); document.head.appendChild(new Style({
-								"@Keyframes SEIGPC-Loading-Spinner__Anime": {
-									"0%": {
-										"Transform": "Rotate(0deg)"
-									},
+							});
 
-									"100%": {
-										"Transform": "Rotate(360deg)"
-									}
-								},
-
-								"SEIGPC-Loading > SEIGPC-Loading-Spinner": {
-									"Display": "Block",
-									"Width": "50px",
-									"Height": "50px",
-
-									"Margin": "Auto",
-									"Background-Position": "Center",
-									"Background-Size": "Cover",
-
-									"Z-Index": 102
-								},
-
-								"SEIGPC-Loading > SEIGPC-Loading-Spinner.is-active": {
-									"Animation": "SEIGPC-Loading-Spinner__Anime 1s Ease-In-Out 0s Infinite Normal"
-								}
-							}));
-
-							return Spinner;
+							return Card;
 						})(),
 
 						enumerable: true
 					}
 				});
 
-				return Loading;
+				return Profile;
 			})(),
 
 			enumerable: true
@@ -210,10 +291,22 @@ const SEIGPC = (function () {
 			});
 		});
 
-		document.body.appendChild(new SEIGPC.Loading());
+
+
+		let loadingScreen = DOM("SEIGPC-Loading", {
+			children: [
+				DOM("SEIGPC-Loading-Spinner", {
+					styles: {
+						"--Url": 'URL("favicon.png")'
+					}
+				})
+			]
+		});
+
+		document.body.appendChild(loadingScreen);
 	});
 
 
-	
+
 	return SEIGPC;
 })();
