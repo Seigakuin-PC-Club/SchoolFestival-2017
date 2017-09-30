10cbe83940098d5afc82811faf6444560b36b73a
Genbu Hase <hasegenbu@gmail.com>
2017-09-30 18:59:21 +0900

    とりま色々。

--- Members/GenbuHase/Breakout/assets/classes/Breakout.js
+++ Members/GenbuHase/Breakout/assets/classes/Breakout.js
@@ -29,18 +29,34 @@ const Breakout = (() => {
 
 					x: { value: 0, configurable: true, writable: true, enumerable: true },
 					y: { value: 0, configurable: true, writable: true, enumerable: true },
-					width: { value: 0, configurable: true, writable: true, enumerable: true },
-					height: { value: 0, configurable: true, writable: true, enumerable: true }
+					texture: { value: null, configurable: true, writable: true, enumerable: true }
 				}); Object.defineProperties(Substance, {
 					Ball: {
 						value: (() => {
 							function Ball () {
 								
 							}; Ball.prototype = Object.create(Substance.prototype, {
-								draw: {
-									value () {
+								constructor: { value: Ball },
+
+								radius: { value: 0, configurable: true, writable: true, enumerable: true },
+
+								move: {
+									value (dx = 0, dy = 0) {
+										this.x += dx,
+										this.y += dy;
+
 										ctx.beginPath();
-										ctx.arc(this.x, this.y, this.width, 0, Math.PI * 2, true);
+										ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+
+										ctx.fillStyle = "RGB(128, 128, 128)";
+										ctx.fill();
+									}
+								},
+
+								draw: {
+									value (x, y, radius) {
+										ctx.beginPath();
+										ctx.arc(x, y, radius, 0, Math.PI * 2, true);
 
 										ctx.fillStyle = "RGB(128, 128, 128)";
 										ctx.fill();
@@ -90,6 +106,48 @@ const Breakout = (() => {
 				return Substance;
 			})()
 		},
+
+		Color: {
+			value: (() => {
+				function Color (red = 0, green = 0, blue = 0) {
+					this.r = red,
+					this.g = green,
+					this.b = blue;
+				}; Color.prototype = Object.create(null, {
+					constructor: { value: Color },
+
+					r: { value: 0, configurable: true, writable: true, enumerable: true },
+					g: { value: 0, configurable: true, writable: true, enumerable: true },
+					b: { value: 0, configurable: true, writable: true, enumerable: true },
+
+					toString: {
+						value () {
+							return `RGB(${this.r}, ${this.g}, ${this.b})`;
+						}
+					}
+				});
+
+				return Color;
+			})()
+		},
+
+		Texture: {
+			value: (() => {
+				function Texture (url = "") {
+					this.src = url,
+					this.image = new Image(url);
+				}; Texture.prototype = Object.create(null, {
+					constructor: { value: Texture },
+
+					src: { value: "", configurable: true, writable: true, enumerable: true },
+					image: { value: new Image(), configurable: true, writable: true, enumerable: true }
+				});
+
+				return Texture;
+			})()
+		},
+
+		
 		
 		width: { value: 0, configurable: true, writable: true, enumerable: true },
 		height: { value: 0, configurable: true, writable: true, enumerable: true },
